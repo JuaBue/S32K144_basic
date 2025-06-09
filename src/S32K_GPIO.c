cd67303bd041ff9ddbf3d9393ee4aaac2ff33040
Juan Bueno <juigbueno@gmail.com>
2025-06-09 17:56:23 +0200

    Include GPIO module in the main with some usage examples and fill the GPIO external interrupt initialization funtion

--- src/S32K_GPIO.c
+++ src/S32K_GPIO.c
@@ -90,5 +90,20 @@ void GPIO_PortPull(PTXn_e ptx_n,uint8_t data)
 
 void GPIO_ExtiInit(PTXn_e ptx_n, exti_cfg cfg)
 {
+    uint8_t ptx,ptn;
 
+    ptx = PTX(ptx_n);
+    ptn = PTn(ptx_n);
+
+    /* Enable port clock */
+    PCC->PCCn[PCC_PORTA_INDEX + ptx] = PCC_PCCn_CGC_MASK;
+
+    /* Clear interrupt flag */
+    PORTX[ptx]->ISFR = (uint32_t)(1 << ptn);
+
+    /* Configuring Port Functions */
+    PORTX[ptx]->PCR[ptn] = PORT_PCR_MUX(1) | PORT_PCR_IRQC(cfg & 0x7f ) | PORT_PCR_PE_MASK | ((cfg & 0x80 ) >> 7);
+
+    /* Set the port to input */
+    GPIOX[ptx]->PDDR &= ~(uint32_t)(1 << ptn);
 }
