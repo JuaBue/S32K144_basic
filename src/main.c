1cf048fca1020a3c2cb83fc5c4b151f76a9c06ed
Juan Bueno <juigbueno@gmail.com>
2025-07-28 11:33:54 +0200

    Update file formating for main.c #7

--- src/main.c
+++ src/main.c
@@ -1,6 +1,31 @@
 /*
- * main implementation: use this 'C' sample to create your own application
+ * =============================================================================
+ * File Name    : main.c
+ * Project      : S32K144_basic
+ * Module       : Application Entry Point
+ * Author       : JuaBue
+ * Created On   : 2025-03-13
+ * Version      : 1.0.0
  *
+ * Description  :
+ *   Entry point for testing and validating all modules within the S32K144_basic
+ *   project. This file contains application-level code used to verify GPIO,
+ *   LED, UART, KEY input, and PLL functionalities on the NXP S32K144 platform.
+ *
+ * License :
+ *   This file is part of a free software project released under the terms of
+ *   the GNU General Public License version 3 (GPLv3).
+ *
+ *   You are free to use, modify, and distribute this file under the conditions
+ *   of the GPLv3, as long as you retain this header and provide proper
+ *   attribution to the original author.
+ *
+ *   See <https://www.gnu.org/licenses/gpl-3.0.html> for the full license text.
+ *
+ *   Copyright (c) 2025 Juan I. Bueno
+ *   All rights reserved.
+ *
+ * =============================================================================
  */
 
 //==============================================================================
